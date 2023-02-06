73a2c49f496a788a4f32dc59e02a35c9176ec99b
Alexander Espinas <90251269+ft-Alexander@users.noreply.github.com>
2023-02-06 18:01:20 +0800

    Update
    Added 2 tabs contact and testimonials

--- script.js
+++
@@ -1,6 +0,0 @@
-const togglebutton = documen.getElementByClassName('toggle-button')[0]
-const navbarLinks = document.getElementsByClassName('nav-links')[0]
-
-togglebutton.addEventListener('click',()=>{
-    navbarLinks.classList.toggle("active")
-})
